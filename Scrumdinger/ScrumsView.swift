926484444ad587fd7632c837e53d1ded8dd95fbe
JackyShen <mebusw@163.com>
2021-02-13 14:41:20 +0800

    feat: implemented "Creating a Navigation Hierarchy" in https://developer.apple.com/tutorials/app-dev-training/creating-a-navigation-hierarchy

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -13,15 +13,20 @@ struct ScrumsView: View {
     var body: some View {
         List {
             ForEach(scrums) { scrum in
-                CardView(scrum: scrum)
-                    .listRowBackground(scrum.color)
+                NavigationLink(destination: DetailView(scrum: scrum)) {
+                    CardView(scrum: scrum)
+                }
+                .listRowBackground(scrum.color)
             }
         }
+        .navigationTitle("Daily Scrums")
     }
 }
 
 struct ScrumsView_Previews: PreviewProvider {
     static var previews: some View {
-        ScrumsView(scrums: DailyScrum.data)
+        NavigationView {
+            ScrumsView(scrums: DailyScrum.data)
+        }
     }
 }
