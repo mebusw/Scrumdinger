5eec62ac6a1c43fa30bac7b316a5f7a627c55e13
JackyShen <mebusw@163.com>
2021-02-13 15:38:55 +0800

    feat: implemented "Passing Data with Bindings" in https://developer.apple.com/tutorials/app-dev-training/passing-data-with-bindings

--- Scrumdinger/CardView.swift
+++ Scrumdinger/CardView.swift
@@ -11,12 +11,18 @@ struct CardView: View {
     let scrum: DailyScrum
     var body: some View {
         VStack(alignment: .leading) {
-            Text(scrum.title).font(.headline)
+            Text(scrum.title)
+                .font(.headline)
             Spacer()
             HStack {
-                Label("\(scrum.attendees.count)", systemImage: "person.3").accessibilityElement(children: .ignore).accessibilityLabel(Text("Attendees")).accessibilityValue(Text("\(scrum.attendees.count)"))
+                Label("\(scrum.attendees.count)", systemImage: "person.3")
+                    .accessibilityElement(children: .ignore)
+                    .accessibilityLabel(Text("Attendees"))
+                    .accessibilityValue(Text("\(scrum.attendees.count)"))
                 Spacer()
-                Label("\(scrum.lengthInMinutes)", systemImage: "clock").padding(.trailing, 20).accessibilityElement(children: .ignore)
+                Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                    .padding(.trailing, 20)
+                    .accessibilityElement(children: .ignore)
                     .accessibilityLabel(Text("Meeting length"))
                     .accessibilityValue(Text("\(scrum.lengthInMinutes) minutes"))
             }
@@ -30,6 +36,8 @@ struct CardView: View {
 struct CardView_Previews: PreviewProvider {
     static var scrum = DailyScrum.data[0]
     static var previews: some View {
-        CardView(scrum: scrum).background(scrum.color).previewLayout(.fixed(width: 400, height: 60))
+        CardView(scrum: scrum)
+            .background(scrum.color)
+            .previewLayout(.fixed(width: 400, height: 60))
     }
 }
--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -9,33 +9,36 @@ import SwiftUI
 
 struct MeetingView: View {
     var body: some View {
-        //        Text("Hello, world!").padding()
         VStack {
             ProgressView(value: 5, total: 15)
             HStack {
-                VStack(alignment: .leading){
-                    Text("Seconds Elapsed").font(.caption)
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
                     Label("300", systemImage: "hourglass.bottomhalf.fill")
                 }
                 Spacer()
-                VStack(alignment: .trailing){
-                    Text("Seconds Remaining").font(.caption)
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
                     Label("600", systemImage: "hourglass.tophalf.fill")
                 }
             }
             .accessibilityElement(children: .ignore)
             .accessibilityLabel(Text("Time remaining"))
             .accessibilityValue(Text("10 minutes"))
-            Circle().strokeBorder(lineWidth: 24, antialiased: true)
-            HStack{
-                Text("Speaker 1 of 3.")
+            Circle()
+                .strokeBorder(lineWidth: 24, antialiased: true)
+            HStack {
+                Text("Speaker 1 of 3")
                 Spacer()
-                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
-                    Image(systemName: "forward.fill")
+                Button(action: {}) {
+                    Image(systemName:"forward.fill")
                 }
                 .accessibilityLabel(Text("Next speaker"))
             }
-        }.padding()
+        }
+        .padding()
     }
 }
 
